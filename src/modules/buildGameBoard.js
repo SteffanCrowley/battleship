7c1bd254610579b242f76deb555f38973a8efdeb
SteffanCrowley <steffancrow@gmail.com>
2022-06-20 13:33:39 -0700

    Add color for different markers on UI

--- src/modules/buildGameBoard.js
+++ src/modules/buildGameBoard.js
@@ -5,15 +5,29 @@ const container = document.createElement("div");
 container.classList.add("container");
 main.appendChild(container);
 
-function buildGameBoard() {
+function buildGameBoard(array) {
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
       square.style.width = `${50}px`;
       square.style.height = `${50}px`;
+      let mark = array[i][j];
+      paintBoard(mark, square);
       container.append(square);
     }
+  }
+}
+
+function paintBoard(mark, square) {
+  if (mark == undefined) {
+    square.classList.add("empty");
+  } else if (mark == "X") {
+    square.classList.add("hit");
+  } else if (mark == "M") {
+    square.classList.add("miss");
+  } else if (mark == "P" || mark == "C" || mark == "B") {
+    square.classList.add("ship");
   }
 }
 
--- src/modules/buildPage.js
+++ src/modules/buildPage.js
@@ -1,4 +1,6 @@
 import { buildGameBoard } from "./buildGameBoard";
+import Board from "./board";
+import Ship from "./ship";
 
 function buildHeader() {
   const element = document.createElement("div");
@@ -14,8 +16,23 @@ function buildFooter() {
 }
 
 function build() {
+  const newBoard = Board();
+  let oneArray = newBoard.make2dArray(10, 10);
+  const patrol = Ship(2, "P");
+  const cruiser = Ship(3, "C");
+  const battleship = Ship(4, "B");
+
+  newBoard.placeShip(patrol, 0, 0, "-y", oneArray);
+  newBoard.placeShip(cruiser, 1, 0, "-y", oneArray);
+  newBoard.placeShip(battleship, 2, 0, "-y", oneArray);
+
+  newBoard.receiveAttack(0, 0, oneArray);
+  newBoard.receiveAttack(0, 1, oneArray);
+  newBoard.receiveAttack(5, 5, oneArray);
+  newBoard.receiveAttack(9, 9, oneArray);
+
   buildHeader();
-  buildGameBoard();
+  buildGameBoard(oneArray);
   buildFooter();
 }
 
